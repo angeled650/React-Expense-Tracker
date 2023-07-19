0a1df5cbd28686366552fc38fb4a95fcf3883eee
angeled650 <angeleduardoch11@gmail.com>
2023-07-19 17:16:04 -0400

    Agregando diseño responsive

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import { GlobalProvider } from "./context/GlobalState";
 function App() {
   return (
     <GlobalProvider>
-      <div className="bg-zinc-950 text-white h-screen flex justify-center items-center">
-        <div className="container mx-auto w-3/6">
-          <div className="bg-zinc-800 p-10 rounded-lg flex gap-x-2">
+      <div className="bg-zinc-950 text-white h-screen flex justify-center items-center overflow-auto">
+        <div className="container mx-auto my-auto py-5 w-3/6 ">
+          <div className="bg-zinc-800 p-10 rounded-lg flex gap-x-2 flex-col md:flex-row">
             <div>
               <h1 className="text-4xl font-bold">Registro Finanzas</h1>
               <IncomeExpenses />
